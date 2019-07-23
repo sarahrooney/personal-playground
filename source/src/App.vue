14a3df71ae6451d75f69794be66cb89930ba11c9
Sarah Rooney <rooney.sarah2@gmail.com>
2019-07-23 16:54:03 -0500

    new in prog page

--- source/src/App.vue
+++ source/src/App.vue
@@ -2,6 +2,18 @@
   <div id="app">
 
     <div class="home">
+      <div class="home__bubbles">
+        <sequential-entrance animation="fadeIn" delay="1600">
+          <div class="home__bubble" v-for="(item, index) in messages" :key="index">
+            <div class="home__bubble__tail"
+              v-bind:class="item.last ? 'home__bubble__tail--active' : 'home__bubble__tail--inactive'"
+              v-bind:style="{ animationDelay: index * 1500 + 'ms' }">
+            </div>
+            <span v-html="item.msg">
+            </span>
+          </div>
+        </sequential-entrance>
+      </div>
     </div>
 
   </div>
@@ -12,7 +24,18 @@ export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      messages: [
+        {
+          msg: "hi! my name is Sarah Rooney. i'm a front-end developer and artist currently living and working in Chicago."
+        },
+        {
+          msg: "i've been reworking my site for a while now, sorry about that! in the meantime, check out my most recent work with the Media Archaeology Lab <a href='https://mediaarchaeologylab.com/blog/soft-computers-crochet-patterns-by-sarah-rooney/' target='_blank'>here</a>."
+        },
+        {
+          msg: "my CV is available <a href='https://docs.google.com/document/d/1xBTE-XqEK6XkJDirgz8oyHLrDkxgKjRqUS8ZSMpZAf8/edit?usp=sharing' target='_blank'>here</a> and you can shoot me an email <a href='mailto:rooney.sarah2@gmail.com'>here</a>.",
+          last: true
+        }
+      ]
     }
   },
   methods: {
--- source/src/main.js
+++ source/src/main.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import SequentialEntrance from 'vue-sequential-entrance'
+import 'vue-sequential-entrance/vue-sequential-entrance.css'
+
+Vue.use(SequentialEntrance);
 
 new Vue({
   el: '#app',
