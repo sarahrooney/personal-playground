617c1bcd5a3f92f8b631e73181483d490728325a
Sarah Rooney <rooney.sarah2@gmail.com>
2019-07-24 14:07:05 -0500

    get rid of webpack, use parcel

--- source/postcss.config.js
+++
@@ -1,7 +0,0 @@
-isProduction =  ( process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging' )
-
-module.exports = {
-  plugins: isProduction ? [
-    require('autoprefixer')
-  ] : []
-}
