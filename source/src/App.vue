374ec00f9296fe6792e546e0766962df26b278ab
Sarah Rooney <rooney.sarah2@gmail.com>
2021-03-08 10:45:17 -0600

    replace links again lol

--- source/src/App.vue
+++ source/src/App.vue
@@ -29,7 +29,7 @@ export default {
           msg: "hi! i'm a front-end developer and artist currently living and working in Chicago, IL ^___^"
         },
         {
-          msg: "i've been reworking my site for a while now, sorry about that! in the meantime, check out my work with the Media Archaeology Lab <a href='https://drive.google.com/file/d/13wAaHNJNJo7LuuO1LgN4K1sGpFsjB1gZ/view?usp=sharing' target='_blank'>here</a> & <a href='https://www.mediaarchaeologylab.com/projects'>here</a>."
+          msg: "i've been reworking my site for a while now, sorry about that! in the meantime, check out <a href='https://drive.google.com/file/d/13wAaHNJNJo7LuuO1LgN4K1sGpFsjB1gZ/view?usp=sharing' target='_blank'>my work</a> with the <a href='https://www.mediaarchaeologylab.com/projects' target='_blank'>Media Archaeology Lab</a>."
         },
         {
           msg: "my CV is available <a href='https://docs.google.com/document/d/1xBTE-XqEK6XkJDirgz8oyHLrDkxgKjRqUS8ZSMpZAf8/edit?usp=sharing' target='_blank'>here</a> and you can shoot me an email <a href='mailto:rooney.sarah2@gmail.com'>here</a>.",
