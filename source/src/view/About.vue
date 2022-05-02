883191a0af8d22cc2380eae5050b14c389fa008c
Sarah Rooney <rooney.sarah2@gmail.com>
2022-05-02 09:32:12 -0500

    add CV

--- source/src/view/About.vue
+++ source/src/view/About.vue
@@ -4,6 +4,7 @@
     <div class="about__cntnt">
       <div class="txt">
         <h2>27/shethey/chicago</h2>
+        <a class="inline-link cv" href="https://docs.google.com/document/d/1xBTE-XqEK6XkJDirgz8oyHLrDkxgKjRqUS8ZSMpZAf8/edit?usp=sharing" target="_blank"><span>˗ˋ⊹CV⊹ˊ˗</span></a>
         <p>my work explores the correlations between personal identity and personalized technology.</p>
         <p>i'm also a fontend web developer -- some clients I've worked with in the past include Patagonia, Saucony, and the Apollo Theater in New York City. i'm currently working at <a href="//cliquestudios.com/" target="_blank"><span>Clique Studios</span></a>.</p>
         <p>i've collaborated with the folks at <a href="//netizen.org/" target="_blank"><span>netizen.org</span></a> designing & styling <a href="//netnet.studio/" target="_blank"><span>netnet.studio</span></a>, an open source hypermedia tool for learning and playing with code in the browser.</p>
